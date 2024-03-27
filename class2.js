a82e2bafe4e7056d5c9ac749f61d95d7295e4751
abdullateefyusuf <yusufolusesiabdullateef@gmail.com>
2024-03-27 22:40:43 +0100

    javascriptday2

---
+++ class2.js
@@ -0,0 +1,103 @@
+/*let student =' ade ';
+let age = 23;
+let height = 56;
+
+function  mystudent () {
+    console.log('age')
+    console.log('my name is ' + student)
+}
+mystudent()
+
+
+let firstName = 'Tunde';
+let lastName = 'Janet';
+
+console.log(firstName + lastName)
+console.log(student +' is ' + age + ' years old')
+
+
+//block scope or local scope
+
+function greet() {
+    let someone = 'Good Morning';
+    let person = 'How are you';
+    console.log(someone + person)
+    console.log(firstName)
+}
+greet()*/
+
+
+
+let present = true;
+let pupil = 'john';
+let height = 53.4;
+
+
+let schools = ['Top-Trend', 'Ebunoluwa', 'Oscp']
+let availabledata = {pupilName: 'Tade', school: 'OIC', age:12, height:'56'}
+
+console.log(typeof(present))
+console.log(typeof(pupil))
+console.log(typeof(height))
+console.log(typeof(schools))
+console.log(schools.length)
+console.log(availabledata)
+
+
+console.log('5'+'5')
+
+let x = 50;
+let y = 12;
+let a = x + y;
+let z = x - y;
+let b = x / y;
+console.log(z) 
+console.log(b)
+console.log(a)
+
+
+
+
+//assignment operator
+
+let m = 5;
+m += 34;
+console.log(m)
+
+
+
+//camparison
+
+let d = 20;
+let u = 40;
+if (a != b){
+    console.log('HI!')
+} else{
+    console.log('error')
+}
+
+
+
+
+let shoe = 'Gucci';
+let bag = 'Tote ';
+let accessory = bag + shoe;
+console.log(accessory)
+
+
+
+let r = '9';
+let g = '5';
+let h = r > g;
+console.log(h)
+
+
+//logical
+
+let s = 10;
+let v = 12;
+if (s == g) {
+    console.log('HAPPY')
+}else {
+    console.log(false)
+}
